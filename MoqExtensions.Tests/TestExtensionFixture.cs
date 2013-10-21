32a12534d02be82dd1fad410a56205f4fa895acd
jonathan.baseheart <jonathan.baseheart@corvisa.com>
2013-10-21 15:54:29 -0500

    initial commit

---
+++ MoqExtensions.Tests/TestExtensionFixture.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using Moq;
+using MoqExtensions;
+
+namespace PipeFire2.Core.Tests
+{
+    [TestFixture]
+    public class TestExtensionFixture
+    {
+
+        [Test]
+        public void Test_FluentInterface()
+        {
+
+            var repo = GetFakeRepository()
+                .WhereEntity(r => r.Office)
+                    .ReturnsMockWith(ofc => ofc.OfficeId, 4444,
+                                     ofc => ofc.OfficeName, "AdventureWorks")
+                .WhereEntity(r => r.Order)
+                    .ReturnsMockWith(o => o.OrderId, 1234, o => o.OfficeId, 4444);
+
+            Assert.That(repo.Object.Order.First().OrderId, Is.EqualTo(1234));
+            Assert.That(repo.Object.Order.First().OfficeId, Is.EqualTo(4444));
+            Assert.That(repo.Object.Office.First().OfficeId, Is.EqualTo(4444));
+            Assert.That(repo.Object.Office.First().OfficeName, Is.EqualTo("AdventureWorks"));
+        }
+
+
+
+
+        [Test]
+        public void Test_GetEnumerableFake()
+        {
+            var oe = MockHelpers.GetEnumerableFake<OrderEntity, int>(o => o.OrderId, 1234);
+            Assert.That(oe.First().OrderId, Is.EqualTo(1234));
+        }
+
+
+        private Mock<ITestRepository> GetFakeRepository()
+        {
+            return new Mock<ITestRepository>();
+        }
+
+    }
+
+
+    #region Test Entities
+
+    public interface ITestRepository
+    {
+        IQueryable<OrderEntity> Order { get; }
+        IQueryable<OfficeEntity> Office { get; }
+    }
+
+    public class OrderEntity
+    {
+        public virtual int OrderId { get; set; }
+        public virtual int OfficeId { get; set; }
+    }
+
+    public class OfficeEntity
+    {
+        public virtual int OfficeId { get; set; }
+        public virtual string OfficeName { get; set; }
+    }
+
+    public class EmployeeEntity
+    {
+        public virtual int EmployeeId { get; set; }
+        public virtual string EmployeeName { get; set; }
+        public virtual int OfficeId { get; set; }
+    }
+
+
+    #endregion
+
+}
---
+++ MoqExtensions/TestExtensions.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using Moq;
+using Moq.Language.Flow;
+
+namespace MoqExtensions
+{
+
+    public static class MockHelpers
+    {
+
+
+        public class SetupWrapper<TRepo, TEntity>
+            where TRepo : class
+            where TEntity : class
+        {
+            public Mock<TRepo> repo;
+            public ISetup<TRepo, IQueryable<TEntity>> setup;
+        }
+
+
+
+        public static SetupWrapper<TRepo, TEntity> WhereEntity<TRepo, TEntity>(this Mock<TRepo> source,
+            Expression<Func<TRepo, IQueryable<TEntity>>> expr)
+            where TRepo : class
+            where TEntity : class
+        {
+            var wrapper = new SetupWrapper<TRepo, TEntity>();
+            wrapper.repo = source;
+            wrapper.setup = wrapper.repo.Setup(expr);
+            return wrapper;
+        }
+
+
+        #region ReturnsMockWith
+
+        public static Mock<TRepo> ReturnsMockWith<TRepo, TEntity, TValue>(this SetupWrapper<TRepo, TEntity> where,
+            Expression<Func<TEntity, TValue>> expr, TValue value)
+            where TRepo : class
+            where TEntity : class
+        {
+            var mockEntity = GetMock<TEntity>();
+            mockEntity.Setup(expr).Returns(value);
+
+            where.setup.Returns(mockEntity.AsEnumerable().AsQueryable());
+
+            return where.repo;
+        }
+
+
+        public static Mock<TRepo> ReturnsMockWith<TRepo, TEntity, TValue, TValue2>(this SetupWrapper<TRepo, TEntity> where,
+            Expression<Func<TEntity, TValue>> expr1, TValue value1,
+            Expression<Func<TEntity, TValue2>> expr2, TValue2 value2)
+            where TRepo : class
+            where TEntity : class
+        {
+            var mockEntity = GetMock<TEntity>();
+            mockEntity.Setup(expr1).Returns(value1);
+            mockEntity.Setup(expr2).Returns(value2);
+
+            where.setup.Returns(mockEntity.AsEnumerable().AsQueryable());
+
+            return where.repo;
+        }
+
+        public static Mock<TRepo> ReturnsMockWith<TRepo, TEntity, TValue, TValue2, TValue3>(this SetupWrapper<TRepo, TEntity> where,
+            Expression<Func<TEntity, TValue>> expr1, TValue value1,
+            Expression<Func<TEntity, TValue2>> expr2, TValue2 value2,
+            Expression<Func<TEntity, TValue3>> expr3, TValue3 value3)
+            where TRepo : class
+            where TEntity : class
+        {
+            var mockEntity = GetMock<TEntity>();
+            mockEntity.Setup(expr1).Returns(value1);
+            mockEntity.Setup(expr2).Returns(value2);
+            mockEntity.Setup(expr3).Returns(value3);
+
+            where.setup.Returns(mockEntity.AsEnumerable().AsQueryable());
+
+            return where.repo;
+        }
+
+
+        public static Mock<TRepo> ReturnsMockWith<TRepo, TEntity, TValue, TValue2, TValue3, TValue4>(this SetupWrapper<TRepo, TEntity> where,
+            Expression<Func<TEntity, TValue>> expr1, TValue value1,
+            Expression<Func<TEntity, TValue2>> expr2, TValue2 value2,
+            Expression<Func<TEntity, TValue3>> expr3, TValue3 value3,
+            Expression<Func<TEntity, TValue4>> expr4, TValue4 value4)
+            where TRepo : class
+            where TEntity : class
+        {
+            var mockEntity = GetMock<TEntity>();
+            mockEntity.Setup(expr1).Returns(value1);
+            mockEntity.Setup(expr2).Returns(value2);
+            mockEntity.Setup(expr3).Returns(value3);
+            mockEntity.Setup(expr4).Returns(value4);
+
+            where.setup.Returns(mockEntity.AsEnumerable().AsQueryable());
+
+            return where.repo;
+        }
+
+        #endregion
+
+
+
+
+
+
+        /// <summary>Usage:
+        /// var a = MockHelpers.GetMock&lt;TEntity&gt;().WithValue(t => t.idField, 1234).WithValue(t => t.stringField, "name")
+        /// </summary>
+        /// <typeparam name="TEntity"></typeparam>
+        /// <returns></returns>
+        public static Mock<TEntity> GetMock<TEntity>() where TEntity : class
+        {
+            return new Mock<TEntity>();
+        }
+
+        public static Mock<TEntity> WithValue<TEntity, TValue>(this Mock<TEntity> source, System.Linq.Expressions.Expression<Func<TEntity, TValue>> expr, TValue value) where TEntity : class
+        {
+            source.Setup(expr).Returns(value);
+            return source;
+        }
+
+        public static IEnumerable<TEntity> AsEnumerable<TEntity>(this Mock<TEntity> source) where TEntity : class
+        {
+            yield return source.Object;
+        }
+
+
+        public static IEnumerable<TEntity> GetEnumerableFake<TEntity, TValue>(Expression<Func<TEntity, TValue>> expr, TValue value)
+            where TEntity : class
+        {
+            return GetMock<TEntity>().WithValue(expr, value).AsEnumerable();
+        }
+
+    }
+
+
+}
